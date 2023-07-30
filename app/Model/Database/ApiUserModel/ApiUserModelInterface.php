70d31cb1052121870529021152dbef84e2e5793c
yusam <yusam@yandex.ru>
2023-07-30 14:31:36 +0300

    #changed

--- app/Model/Database/ApiUserModel/ApiUserModelInterface.php
+++ app/Model/Database/ApiUserModel/ApiUserModelInterface.php
@@ -28,10 +28,10 @@ interface ApiUserModelInterface
 {
     const ATTRIBUTE_NAME_ID = 'id';
     const ATTRIBUTE_NAME_API_TOKEN = 'apiToken';
-    const ATTRIBUTE_NAME_API_SIGN = '$apiSign';
-    const ATTRIBUTE_NAME_DESCRIPTION = '$description';
-    const ATTRIBUTE_NAME_BLOCKED_AT = '$blockedAt';
-    const ATTRIBUTE_NAME_BLOCKED_DESCRIPTION = '$blockedDescription';
+    const ATTRIBUTE_NAME_API_SIGN = 'apiSign';
+    const ATTRIBUTE_NAME_DESCRIPTION = 'description';
+    const ATTRIBUTE_NAME_BLOCKED_AT = 'blockedAt';
+    const ATTRIBUTE_NAME_BLOCKED_DESCRIPTION = 'blockedDescription';
     const ATTRIBUTE_NAME_CREATED_AT = 'createdAt';
     const ATTRIBUTE_NAME_MODIFIED_AT = 'modifiedAt';
 }
