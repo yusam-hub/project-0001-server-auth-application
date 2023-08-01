1a6edd3303411843062b527ebffb5ae86e54b275
yusam <yusam@yandex.ru>
2023-08-02 02:38:25 +0300

    #changed

--- database/migrations/default/20220103_000002_create_table_apps_users_keys.php
+++ database/migrations/default/20220103_000002_create_table_apps_users_keys.php
@@ -17,18 +17,18 @@ SET FOREIGN_KEY_CHECKS=0;
 DROP TABLE IF EXISTS `:database`.`:table`;
 
 CREATE TABLE IF NOT EXISTS `:database`.`:table` (
-    `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
     `appId` bigint(20) unsigned NOT NULL COMMENT 'Приложение',
     `userId` bigint(20) unsigned NOT NULL COMMENT 'Пользователь',
+    `deviceUuid` varchar(36) NOT NULL COMMENT 'Uuid устройства пользователя'
     `publicKey` text DEFAULT NULL COMMENT 'Публичный ключ',
+    `lastUsedAt` datetime NULL  DEFAULT NULL COMMENT 'Дата последнего использования ключа',
     `createdAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Дата создания записи',
     `modifiedAt` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT 'Дата изменения записи',
     PRIMARY KEY (`id`),
-    KEY `idx_userId` (`userId`) USING BTREE,
-    UNIQUE KEY `idx_appId_userId` (`appId`,`userId`) USING BTREE,
+    UNIQUE KEY `idx_appId_userId_deviceUuid` (`appId`,`userId`,`deviceUuid`) USING BTREE,
     CONSTRAINT `fkAppId_:table` FOREIGN KEY (`appId`) REFERENCES `apps` (`id`),
     CONSTRAINT `fkUserId_:table` FOREIGN KEY (`userId`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Ключи пользователей для приложений';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Ключи на устройства пользователей для приложений';
 
 SET FOREIGN_KEY_CHECKS=1;
 MYSQL;
