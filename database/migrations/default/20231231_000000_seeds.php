dc9dca7e4e05e859650a24bb7df3abe3e2f72550
yusam <yusam@yandex.ru>
2023-08-01 09:18:46 +0300

    #changed

--- database/migrations/default/20231231_000000_seeds.php
+++ database/migrations/default/20231231_000000_seeds.php
@@ -11,25 +11,43 @@ return new class {
     public function getQuery(): string
     {
         $query = <<<MYSQL
+SET FOREIGN_KEY_CHECKS=0;
+
 SET autocommit = 0;
 
 START TRANSACTION;
 
+TRUNCATE `:database`.`:table_emails`;
+
 INSERT INTO `:database`.`:table_emails` (`id`,`email`,`verifiedAt`) VALUES(1,'yusam@yandex.ru',NOW());
+
+TRUNCATE `:database`.`:table_country_mobile_prefixes`;
 
 INSERT INTO `:database`.`:table_country_mobile_prefixes` (`id`,`countryCode2`,`countryCode3`,`mobilePrefix`) VALUES(1,'RU','RUS','+7'),(2,'IN','IND','+91'),(3,'UA','UKR','+380');
 
+TRUNCATE `:database`.`:table_mobiles`;
+
 INSERT INTO `:database`.`:table_mobiles` (`id`,`countryMobilePrefixId`,`num`,`verifiedAt`) VALUES(1,1,'9376448660',NOW());
+
+TRUNCATE `:database`.`:table_users`;
 
 INSERT INTO `:database`.`:table_users` (`id`) VALUES(1);
 
+TRUNCATE `:database`.`:table_users_emails`;
+
 INSERT INTO `:database`.`:table_users_emails` (`id`,`userId`,`emailId`) VALUES(1,1,1);
 
+TRUNCATE `:database`.`:table_users_mobiles`;
+
 INSERT INTO `:database`.`:table_users_mobiles` (`id`,`userId`,`mobileId`) VALUES(1,1,1);
+
+TRUNCATE `:database`.`:table_apps`;
 
 INSERT INTO `:database`.`:table_apps` (`id`,`userId`,`title`) VALUES(1,1,'yusam@yandex.ru');
 
 COMMIT;
+
+SET FOREIGN_KEY_CHECKS=1;
 MYSQL;
 
         return strtr($query, [
