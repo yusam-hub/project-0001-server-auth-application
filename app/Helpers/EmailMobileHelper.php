b68fbded0b60f9437df7854e1427530289f39baa
yusam <yusam@yandex.ru>
2023-08-03 14:03:13 +0300

    #changed

--- app/Helpers/EmailMobileHelper.php
+++ app/Helpers/EmailMobileHelper.php
@@ -2,14 +2,32 @@
 
 namespace App\Helpers;
 
+use YusamHub\DbExt\Interfaces\PdoExtKernelInterface;
+
 class EmailMobileHelper
 {
+    /**
+     * @param string $value
+     * @return bool
+     */
     public static function isEmail(string $value): bool
     {
         return filter_var(strtolower($value), FILTER_VALIDATE_EMAIL);
     }
 
-    public static function isMobile(string $value): bool
+    /**
+     * @param PdoExtKernelInterface $pdoExtKernel
+     * @param string $value
+     * @param $mobilePrefix
+     * @param $num
+     * @return bool
+     */
+    public static function isMobile(
+        PdoExtKernelInterface $pdoExtKernel,
+        string $value,
+        &$mobilePrefix,
+        &$num
+    ): bool
     {
         /**
          * todo: 1) нужно подгрузить префиксы стран
