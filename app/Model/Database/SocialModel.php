3df1850bd26f6c1a1580cddaf287a67976acedaa
yusam <yusam@yandex.ru>
2023-08-08 12:30:39 +0300

    #changed

--- app/Model/Database/SocialModel.php
+++ app/Model/Database/SocialModel.php
@@ -2,7 +2,9 @@
 
 namespace App\Model\Database;
 
+use Psr\Log\LoggerInterface;
 use YusamHub\AppExt\Redis\RedisCacheUseFresh;
+use YusamHub\AppExt\Redis\RedisKernel;
 use YusamHub\DbExt\Interfaces\PdoExtKernelInterface;
 use YusamHub\Helper\ArrayHelper;
 
@@ -20,8 +22,11 @@ use YusamHub\Helper\ArrayHelper;
  */
 class SocialModel extends BasePdoExtModel
 {
-    protected ?string $connectionName = DB_CONNECTION_DEFAULT;
-    protected string $tableName = TABLE_SOCIALS;
+    const CURRENT_CONNECTION_NAME =  DB_CONNECTION_DEFAULT;
+    const CURRENT_TABLE_NAME = TABLE_SOCIALS;
+
+    protected ?string $connectionName = self::CURRENT_CONNECTION_NAME;
+    protected string $tableName = self::CURRENT_TABLE_NAME;
 
     const ATTRIBUTE_NAME_ID = 'id';
     const ATTRIBUTE_NAME_ABBR = 'abbr';
@@ -37,5 +42,99 @@ class SocialModel extends BasePdoExtModel
     protected function getDatabaseName(): string
     {
         return app_ext_config('database.connections.'.$this->getConnectionName().'.dbName');
+    }
+
+    /**
+     * @param RedisKernel $redisKernel
+     * @param LoggerInterface $logger
+     * @param PdoExtKernelInterface $pdoExtKernel
+     * @param bool $cacheUse
+     * @param bool $cacheFresh
+     * @param int $cacheTtl
+     * @return array
+     */
+    public static function getAll(
+        RedisKernel $redisKernel,
+        LoggerInterface $logger,
+        PdoExtKernelInterface $pdoExtKernel,
+        bool $cacheUse = true,
+        bool $cacheFresh = false,
+        int $cacheTtl = RedisCacheUseFresh::CACHE_TTL_DAY
+    ): array
+    {
+        return RedisCacheUseFresh::rememberExt(
+            $redisKernel->redisExt(),
+            $logger,
+            md5(__METHOD__),
+            $cacheUse, $cacheFresh, $cacheTtl,
+            function() use($pdoExtKernel) {
+                $sqlRows = <<<MYSQL
+select 
+    id,
+    abbr,
+    title
+from 
+    :current_table_name
+order 
+    by id 
+MYSQL;
+                return $pdoExtKernel
+                    ->pdoExt(self::CURRENT_CONNECTION_NAME)
+                    ->fetchAll(
+                        strtr($sqlRows, [
+                            ':current_table_name' => self::CURRENT_TABLE_NAME
+                        ]),
+                    );
+            });
+    }
+
+    /**
+     * @param RedisKernel $redisKernel
+     * @param LoggerInterface $logger
+     * @param PdoExtKernelInterface $pdoExtKernel
+     * @return array
+     */
+    public static function getAllAbbrToId(
+        RedisKernel $redisKernel,
+        LoggerInterface $logger,
+        PdoExtKernelInterface $pdoExtKernel,
+    ): array
+    {
+        $rows = static::getAll(
+            $redisKernel,
+            $logger,
+            $pdoExtKernel
+        );
+
+        return ArrayHelper::map($rows,
+            'abbr',
+            function($row) {
+                return $row['id'];
+            });
+    }
+
+    /**
+     * @param RedisKernel $redisKernel
+     * @param LoggerInterface $logger
+     * @param PdoExtKernelInterface $pdoExtKernel
+     * @return array
+     */
+    public static function getAllIdToAbbr(
+        RedisKernel $redisKernel,
+        LoggerInterface $logger,
+        PdoExtKernelInterface $pdoExtKernel,
+    ): array
+    {
+        $rows = static::getAll(
+            $redisKernel,
+            $logger,
+            $pdoExtKernel
+        );
+
+        return ArrayHelper::map($rows,
+            'id',
+            function($row) {
+                return $row['abbr'];
+            });
     }
 }
