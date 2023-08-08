7acde8f50a6d4d4a7c8cba407e06f3695d50c139
yusam <yusam@yandex.ru>
2023-08-08 12:27:42 +0300

    #changed

--- app/Model/Database/CountryMobilePrefixModel.php
+++ app/Model/Database/CountryMobilePrefixModel.php
@@ -2,7 +2,11 @@
 
 namespace App\Model\Database;
 
+use Psr\Log\LoggerInterface;
+use YusamHub\AppExt\Redis\RedisCacheUseFresh;
+use YusamHub\AppExt\Redis\RedisKernel;
 use YusamHub\DbExt\Interfaces\PdoExtKernelInterface;
+use YusamHub\Helper\ArrayHelper;
 
 /**
  * @property int $id
@@ -19,8 +23,11 @@ use YusamHub\DbExt\Interfaces\PdoExtKernelInterface;
  */
 class CountryMobilePrefixModel extends BasePdoExtModel
 {
-    protected ?string $connectionName = DB_CONNECTION_DEFAULT;
-    protected string $tableName = TABLE_COUNTRY_MOBILE_PREFIXES;
+    const CURRENT_CONNECTION_NAME =  DB_CONNECTION_DEFAULT;
+    const CURRENT_TABLE_NAME = TABLE_COUNTRY_MOBILE_PREFIXES;
+
+    protected ?string $connectionName = self::CURRENT_CONNECTION_NAME;
+    protected string $tableName = self::CURRENT_TABLE_NAME;
 
     const ATTRIBUTE_NAME_ID = 'id';
     const ATTRIBUTE_NAME_COUNTRY_CODE_2 = 'countryCode2';
@@ -34,5 +41,98 @@ class CountryMobilePrefixModel extends BasePdoExtModel
         return app_ext_config('database.connections.'.$this->getConnectionName().'.dbName');
     }
 
+    /**
+     * @param RedisKernel $redisKernel
+     * @param LoggerInterface $logger
+     * @param PdoExtKernelInterface $pdoExtKernel
+     * @param bool $cacheUse
+     * @param bool $cacheFresh
+     * @param int $cacheTtl
+     * @return array
+     */
+    public static function getAll(
+        RedisKernel $redisKernel,
+        LoggerInterface $logger,
+        PdoExtKernelInterface $pdoExtKernel,
+        bool $cacheUse = true,
+        bool $cacheFresh = false,
+        int $cacheTtl = RedisCacheUseFresh::CACHE_TTL_DAY
+    ): array
+    {
+        return RedisCacheUseFresh::rememberExt(
+            $redisKernel->redisExt(),
+            $logger,
+            md5(__METHOD__),
+            $cacheUse, $cacheFresh, $cacheTtl,
+            function() use($pdoExtKernel) {
+                $sqlRows = <<<MYSQL
+select 
+    id,
+    countryCode2,
+    countryCode3,
+    mobilePrefix
+from 
+    :current_table_name
+order 
+    by id 
+MYSQL;
+                return $pdoExtKernel
+                    ->pdoExt(self::CURRENT_CONNECTION_NAME)
+                    ->fetchAll(
+                        strtr($sqlRows, [
+                            ':current_table_name' => self::CURRENT_TABLE_NAME
+                        ]),
+                    );
+            });
+    }
 
+    /**
+     * @param RedisKernel $redisKernel
+     * @param LoggerInterface $logger
+     * @param PdoExtKernelInterface $pdoExtKernel
+     * @return array
+     */
+    public static function getAllPrefixToId(
+        RedisKernel $redisKernel,
+        LoggerInterface $logger,
+        PdoExtKernelInterface $pdoExtKernel,
+    ): array
+    {
+        $rows = static::getAll(
+            $redisKernel,
+            $logger,
+            $pdoExtKernel
+        );
+
+        return ArrayHelper::map($rows,
+            'mobilePrefix',
+            function($row) {
+                return $row['id'];
+            });
+    }
+
+    /**
+     * @param RedisKernel $redisKernel
+     * @param LoggerInterface $logger
+     * @param PdoExtKernelInterface $pdoExtKernel
+     * @return array
+     */
+    public static function getAllIdToPrefix(
+        RedisKernel $redisKernel,
+        LoggerInterface $logger,
+        PdoExtKernelInterface $pdoExtKernel,
+    ): array
+    {
+        $rows = static::getAll(
+            $redisKernel,
+            $logger,
+            $pdoExtKernel
+        );
+
+        return ArrayHelper::map($rows,
+            'id',
+            function($row) {
+                return $row['mobilePrefix'];
+            });
+    }
 }
